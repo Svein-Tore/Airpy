39cc4fccceb13fca051871b8abcb238113d0fab2
Svein-Tore <svenar89@gmail.com>
2020-12-24 12:05:15 +0100

    Update sender_kod_makekit.py

--- sender_kod_makekit.py
+++ sender_kod_makekit.py
@@ -29,7 +29,7 @@ while True:
     if button_a.is_pressed() and button_b.is_pressed():
         Throttle=0
         Arm=1
-        sendTekst = str(Pitch)+","+str(Roll)+","+str(Throttle)+","+str(Yaw)+","+str(Aux)
+        sendTekst = str(Pitch)+","+str(Roll)+","+str(Throttle)+","+str(Yaw)+","+str(Arm)
         radio.send(sendTekst)
         sleep(1000)
         Yaw=0
@@ -38,5 +38,5 @@ while True:
     display.set_pixel(4, 4, 9)
     display.set_pixel(0, mapping(Throttle, 0, 100, 4, 0), 9)
     display.set_pixel(mapping(Roll, -90, 90, 0, 4), mapping(Pitch, -90, 90, 0, 4), 9)
-    sendTekst = str(Pitch)+","+str(Roll)+","+str(Throttle)+","+str(Yaw)
+    sendTekst = str(Pitch)+","+str(Roll)+","+str(Throttle)+","+str(Yaw)+","+str(Arm)
     radio.send(sendTekst)
